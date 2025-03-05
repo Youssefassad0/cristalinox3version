30c7ff116a99d8bc521cb55e6a79aa2461678a9a
Youssefassad0 <amine150assad@gmail.com>
2025-03-05 16:43:40 +0100

    ajouter cards

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,55 +20,54 @@ function Navbar() {
 
   useEffect(() => {
     showButton();
-  }, [])
+  }, []);
   return (
-    <nav className='navbar'>
-    <div className='navbar-container'>
-      <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-        CristalInox
-       
-      </Link>
-      <div className='menu-icon' onClick={handleClick}>
-        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-      </div>
-      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-        <li className='nav-item'>
-          <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-            Home
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link
-            to='/services'
-            className='nav-links'
-            onClick={closeMobileMenu}
-          >
-            About
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link
-            to='/products'
-            className='nav-links'
-            onClick={closeMobileMenu}
-          >
-            Catalogue
-          </Link>
-        </li>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          CristalInox
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/services"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/products"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Catalogue
+            </Link>
+          </li>
 
-        <li>
-          <Link
-            to='/contact'
-            className='nav-links-mobile'
-            onClick={closeMobileMenu}
-          >
-            Contact
-          </Link>
-        </li>
-      </ul>
-      {button && <Button buttonStyle='btn--outline'>demander devis</Button>}
-    </div>
-  </nav>
+          <li>
+            <Link
+              to="/contact"
+              className="nav-links-mobile"
+              onClick={closeMobileMenu}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+        {button && <Button buttonStyle="btn--outline">demander devis</Button>}
+      </div>
+    </nav>
   );
 }
 
