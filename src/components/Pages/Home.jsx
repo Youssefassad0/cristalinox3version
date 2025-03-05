6bf2aca0d0993bfc0f759b7b670ccf3f4fcd0d87
Youssefassad0 <amine150assad@gmail.com>
2025-03-05 16:57:04 +0100

    ajouter des cards

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../hero/Hero";
+import Cards from "../cardItems/Cards";
 
 function Home() {
   return (
     <>
       <Hero />
+      <Cards />
     </>
   );
 }
