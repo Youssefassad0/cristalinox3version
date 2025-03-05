0d71f8c97d3d08d497155ac5b009e3bc548e8a24
Youssefassad0 <amine150assad@gmail.com>
2025-03-05 15:51:40 +0100

    ajouter background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" element={<Hero />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { Button } from "../navbar/Button";
+import "./Hero.css";
 function Hero() {
   return (
     <div className="hero-container">
-        
+    <video src='/vedios/vedio1.mp4' autoPlay loop muted />
+      <h1>CRISTALINOX</h1>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Demander un Devis
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
