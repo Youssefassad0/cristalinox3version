2f872fa3b0b9c539e98b656514f54a5400934602
Youssefassad0 <amine150assad@gmail.com>
2025-03-06 15:14:23 +0100

    ajouter 2 backgronsds

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "../navbar/Button";
 import { motion } from "framer-motion";
 import "./Hero.css";
 import "../../App.css";
+
 function Hero() {
+  const [videoSource, setVideoSource] = useState("/vedios/vedio2.mp4");
+
+  const handleVideoEnd = () => {
+    if (videoSource === "/vedios/vedio4.mp4") {
+      setVideoSource("/vedios/vedio2.mp4");
+    } else {
+      setVideoSource("/vedios/vedio4.mp4");
+    }
+  };
+
+  useEffect(() => {
+    const videoElement = document.querySelector('video');
+    if (videoElement) {
+      videoElement.load(); 
+    }
+  }, [videoSource]);
+
   return (
     <div className="hero-container">
-      <video src="/vedios/vedio1.mp4" autoPlay loop muted />
+      <video
+        src={videoSource}
+        autoPlay
+        muted
+        onEnded={handleVideoEnd} 
+      />
       <motion.h1
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -14,10 +37,11 @@ function Hero() {
       >
         CristalInox
       </motion.h1>
-      <motion.div className="hero-btns" 
-       initial={{ opacity: 0, x: 100 }}  
-       whileInView={{ opacity: 1, x: 0 }} 
-       transition={{ duration: 1, ease: "easeOut" ,delay :0.8}}
+      <motion.div
+        className="hero-btns"
+        initial={{ opacity: 0, x: 100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, ease: "easeOut", delay: 0.8 }}
       >
         <Button
           className="btns"
