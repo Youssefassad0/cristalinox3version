f03c3892c775e5343f9ab8f0adc9364519ffa630
Youssefassad0 <amine150assad@gmail.com>
2025-03-06 10:29:07 +0100

    ajouter des photos

--- src/components/cardItems/Cards.jsx
+++ src/components/cardItems/Cards.jsx
@@ -10,19 +10,19 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/modern-staircase.jpg"
+              src="images/modern-staircase.jfif"
               text="Custom-designed stainless steel staircases for modern interiors"
               label="Home Decor"
               path="/projects"
             />
             <CardItem
-              src="images/industrial-piping.jpg"
+              src="images/industrial-piping.jfif"
               text="High-quality industrial piping solutions for your business"
               label="Tuyauterie"
               path="/services"
             />
             <CardItem
-              src="images/metal-gate.jpg"
+              src="images/metal-gate.jfif"
               text="Elegant and durable metal gates tailored to your needs"
               label="Ironwork"
               path="/products"
