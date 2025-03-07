217cff653cfcf24f75f666a28a5d438ad89699b2
youssefassad0 <amine150assad@gmail.com>
2025-03-07 09:45:25 +0000

    ajouter des animations au skilss

--- src/components/cardItems/Cards.jsx
+++ src/components/cardItems/Cards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Cards.css";
 import CardItem from "./CardItems";
-
+import { motion } from "framer-motion";
 function Cards() {
   return (
     <div className="cards">
@@ -9,24 +9,48 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem
-              src="images/modern-staircase.jfif"
-              text="Escaliers en acier inoxydable sur mesure pour des intérieurs modernes"
-              label="Décoration intérieure"
-              path="/projets"
-            />
-            <CardItem
-              src="images/industrial-piping.jfif"
-              text="Solutions de tuyauterie industrielle de haute qualité pour votre entreprise"
-              label="Tuyauterie"
-              path="/services"
-            />
-            <CardItem
-              src="images/metal-gate.jfif"
-              text="Portails en métal élégants et durables adaptés à vos besoins"
-              label="Métallerie"
-              path="/produits"
-            />
+            <motion.li
+              className="cards__item"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, ease: "easeOut", delay: 0.3 }}
+              viewport={{ once: true }}
+            >
+              <CardItem
+                src="images/modern-staircase.jfif"
+                text="Escaliers en acier inoxydable sur mesure pour des intérieurs modernes"
+                label="Décoration intérieure"
+                path="/projets"
+              />
+            </motion.li>
+            <motion.li
+              className="cards__item"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, ease: "easeOut", delay: 0.6 }}
+              viewport={{ once: true }}
+            >
+              <CardItem
+                src="images/industrial-piping.jfif"
+                text="Solutions de tuyauterie industrielle de haute qualité pour votre entreprise"
+                label="Tuyauterie"
+                path="/services"
+              />
+            </motion.li>
+            <motion.li
+              className="cards__item"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, ease: "easeOut", delay: 0.9 }}
+              viewport={{ once: true }}
+            >
+              <CardItem
+                src="images/metal-gate.jfif"
+                text="Portails en métal élégants et durables adaptés à vos besoins"
+                label="Métallerie"
+                path="/produits"
+              />
+            </motion.li>
           </ul>
         </div>
       </div>
