a20d343655acb90f91f1fa58d7846ec596520b72
youssefassad0 <amine150assad@gmail.com>
2025-03-08 23:34:23 +0000

    add Slider Correctly

--- src/components/Sliders/Slider.jsx
+++ src/components/Sliders/Slider.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import './Slider.css'
+import React from "react";
+import "./Slider.css";
+
 function Slider() {
+  const images = [
+    "images/p1.jpeg",
+    "images/p2.jpeg",
+    "images/p3.jpeg",
+    "images/p4.jpeg",
+    "images/p5.jpeg",
+    "images/p6.jpeg",
+    "images/p7.jpeg",
+   
+  ];
+
   return (
-   <div className="banner">
-    <div className="slider">
-        <div className="item_slider">
-            <img src="//images/p1.jpeg" alt="photo1" />
+    <div className="Slider_page">
+      <div className="banner">
+        <div className="slider" style={{ "--quantity": images.length }}>
+          {images.map((imgSrc, index) => (
+            <div key={index} className="item_slider" style={{ "--position": index + 1 }}>
+              <img src={imgSrc} alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
         </div>
+        <div className="content">
+          <h1 data-content="Cristal Inox">Cristal Inox</h1>
+          <div className="author">
+            <h2>LUN DEV</h2>
+            <p>
+    High-quality **inox solutions** for <b>decoration, medical, and piping</b> industries.
+  </p>
+  <p>
+    Explore our latest projects and innovations in stainless steel fabrication.
+  </p>
+          </div>
+          <div className="model"></div>
+        </div>
+      </div>
     </div>
-   </div>
-  )
+  );
 }
 
-export default Slider
+export default Slider;
