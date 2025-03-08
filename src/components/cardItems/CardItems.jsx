ee5271f58c20eaa25ad98c8b1961e546df476ad0
youssefassad0 <amine150assad@gmail.com>
2025-03-08 22:25:57 +0000

    modifer le style dfe cards

--- src/components/cardItems/CardItems.jsx
+++ src/components/cardItems/CardItems.jsx
@@ -8,7 +8,7 @@ function CardItem(props) {
         <figure className="cards__item__pic-wrap" data-category={props.label}>
           <img
             className="cards__item__img"
-            alt="Travel Image"
+            alt="cristal inox product"
             src={props.src}
           />
         </figure>
--- src/components/cardItems/Card_Why/Valeurs.jsx
+++ src/components/cardItems/Card_Why/Valeurs.jsx
@@ -4,12 +4,13 @@ import Card_Valeur from "./Card_Valeur";
 function Valeurs() {
   return (
     <div className="cards">
-      <h1>pourquoi Cristal Inox</h1>
+      <h1 className="title-h1" >pourquoi Cristal Inox</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
             <Card_Valeur
               title={"Qualité et Durabilité"}
+              className="cards__item"
               text={
                 "nous utilisons de l'acier inoxydable haut de gamme pour garantir des réalisations solides, esthétiques et durables dans le temps."
               }
@@ -17,12 +18,14 @@ function Valeurs() {
             />
             <Card_Valeur
               title={"Expertise et Savoir-Faire"}
+              className="cards__item"
               text={
                 "Forts d’une expérience en chaudronnerie inox, nous concevons des solutions sur-mesure adaptées aux secteurs de la décoration, du médical et de la tuyauterie."
               }
             />{" "}
             <Card_Valeur
               title={" Innovation et Précision"}
+              className="cards__item"
               text={
                 "Nous allions design et précision technique pour offrir des produits inox qui répondent aux exigences les plus élevées, du simple élément décoratif aux installations industrielles."
               }
