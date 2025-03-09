0e0bd51e7ed252c3f2308138a1bb47a2e41d4dc9
youssefassad0 <amine150assad@gmail.com>
2025-03-09 01:06:46 +0000

    ajouter transitons au router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/Pages/Home";
 import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation(); // Détecte la route actuelle
+
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageTransition><Home /></PageTransition>} />
+        <Route path="/about" element={<PageTransition><About /></PageTransition>} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
-export default App;
+// Animation avec un effet de "révélation"
+function PageTransition({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9, y: 30 }} // Début : réduit et décalé vers le bas
+      animate={{ opacity: 1, scale: 1, y: 0 }} // Fin : normal et centré
+      exit={{ opacity: 0, scale: 1.1, y: -30 }} // Sortie : léger agrandissement et montée
+      transition={{ duration: 0.6, ease: "easeOut" }} // Durée et fluidité
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <AnimatedRoutes />
+      <Footer />
+    </Router>
+  );
+}
