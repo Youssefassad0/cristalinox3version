9fda066ff4f564c883573a2512f8511112766f81
youssefassad0 <amine150assad@gmail.com>
2025-03-13 08:16:35 +0000

    list products good

--- src/Categorie.js
+++ src/Categorie.js
@@ -1,89 +1,123 @@
-const categories = [
+const products = [
   {
-    "nom": "Mobilier et Décoration intérieure",
-    "code": "MDI",
-    "sous-categories": [
+    nom: "Mobilier et Décoration intérieure",
+    code: "MDI",
+    "souscategories": [
       {
-        "nom": "Chaises",
-        "description": "En inox pour un design moderne et épuré.",
-        "produits": [
+        nom: "Chaises",
+        description: "En inox pour un design moderne et épuré.",
+        produits: [
           {
-            "titre": "Chaise moderne en inox",
-            "description": "Une chaise élégante en inox avec un design minimaliste.",
-            "images": [
-              "/public/products/chaises/chaise1.jpeg"
-            ]
+            titre: "Chaise moderne en inox",
+            description:
+              "Une chaise élégante en inox avec un design minimaliste.",
+            images: "/products/chaises/chaise1.jpeg",
           },
           {
-            "titre": "Chaise 2  moderne en inox",
-            "description": "Une chaise élégante en inox avec un design minimaliste.",
-            "images": [
-              "/public/products/chaises/chaise2.jpeg"
-            ]
+            titre: "Chaise 2  moderne en inox",
+            description:
+              "Une chaise élégante en inox avec un design minimaliste.",
+            images: ["/products/chaises/chaise2.jpeg"],
           },
           {
-            "titre": "Chaise 3 moderne en inox",
-            "description": "Une chaise élégante en inox avec un design minimaliste.",
-            "images": "/public/products/chaises/chaise3.jpeg"
-          }
-        ]
+            titre: "Chaise 3 moderne en inox",
+            description:
+              "Une chaise élégante en inox avec un design minimaliste.",
+            images: "/products/chaises/chaise3.jpeg",
+          },
+        ],
       },
       {
-        "nom": "Tables",
-        "description": "Tables à manger, de bureau, ou de café avec des pieds en inox.",
-        "produits": [
+        nom: "Tables",
+        description:
+          "Tables à manger, de bureau, ou de café avec des pieds en inox.",
+        produits: [
           {
-            "titre": "Table à manger en inox",
-            "description": "Une table robuste et moderne avec un plateau en verre et des pieds en inox.",
-            "images": "/public/products/tables/table1.jpeg"
-          }
-        ]
-      }
-    ]
+            titre: "Table à manger en inox",
+            description:
+              "Une table robuste et moderne avec un plateau en verre et des pieds en inox.",
+            images: "/products/tables/table1.jpeg",
+          },
+        ],
+      },
+    ],
   },
   {
-    "nom": "Menuiserie",
-    "code": "Menuiserie",
-    "sous-categories": [
+    nom: "Menuiserie",
+    code: "Menuiserie",
+    "souscategories": [
       {
-        "nom": "Portes",
-        "description": "Portes extérieures ou intérieures en inox, utilisées pour leur robustesse et leur résistance.",
-        "produits": [
+        nom: "Portes",
+        description:
+          "Portes extérieures ou intérieures en inox, utilisées pour leur robustesse et leur résistance.",
+        produits: [
           {
-            "titre": "Porte moderne en inox",
-            "description": "Une porte élégante et résistante pour une sécurité accrue.",
-            "images": [
-              "image7.jpg",
-              "image8.jpg",
-              "image9.jpg"
-            ]
-          }
-        ]
-      }
-    ]
+            titre: "Porte moderne en inox",
+            description:
+              "Une porte élégante et résistante pour une sécurité accrue.",
+            images: "image7.jpg",
+          },
+        ],
+      },
+    ],
   },
   {
-    "nom": "Équipements extérieurs et d'aménagement urbain",
-    "code": "EEAU",
-    "sous-categories": [
+    nom: "Équipements extérieurs et d'aménagement urbain",
+    code: "EEAU",
+    "souscategories": [
       {
-        "nom": "Portes",
-        "description": "Portes extérieures ou intérieures en inox, utilisées pour leur robustesse et leur résistance.",
-        "produits": [
+        nom: "Portes",
+        description:
+          "Portes extérieures ou intérieures en inox, utilisées pour leur robustesse et leur résistance.",
+        produits: [
           {
-            "titre": "Porte moderne en inox",
-            "description": "Une porte élégante et résistante pour une sécurité accrue.",
-            "images": [
-              "image7.jpg",
-              "image8.jpg",
-              "image9.jpg"
-            ]
-          }
-        ]
-      }
-    ]
-  }
-  ];
-  
-  export default categories;
-  
+            titre: "Porte moderne en inox",
+            description:
+              "Une porte élégante et résistante pour une sécurité accrue.",
+            images: "image7.jpg",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    nom: "Équipements sanitaires et industriels",
+    code: "ESI",
+    "souscategories": [
+      {
+        nom: "Portes",
+        description:
+          "Portes extérieures ou intérieures en inox, utilisées pour leur robustesse et leur résistance.",
+        produits: [
+          {
+            titre: "Porte moderne en inox",
+            description:
+              "Une porte élégante et résistante pour une sécurité accrue.",
+            images: "image7.jpg",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    nom: "Composants architecturaux",
+    code: "CA",
+    "souscategories": [
+      {
+        nom: "Portes",
+        description:
+          "Portes extérieures ou intérieures en inox, utilisées pour leur robustesse et leur résistance.",
+        produits: [
+          {
+            titre: "Porte moderne en inox",
+            description:
+              "Une porte élégante et résistante pour une sécurité accrue.",
+            images: "image7.jpg",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default products;
--- src/components/Catalogue/ListProducts.jsx
+++ src/components/Catalogue/ListProducts.jsx
@@ -1,34 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ProductList.css";
-const products = [
-  { id: 1, title: "Chaise Moderne", img: "/products/chaises/chaise1.jpeg" },
-  { id: 1, title: "Chaise Moderne", img: "/products/chaises/chaise2.jpeg" },
-  { id: 1, title: "Chaise Moderne", img: "/products/chaises/chaise3.jpeg" },
-  { id: 2, title: "Table en Verre", img: "/products/tables/table1.jpeg" },
-  { id: 3, title: "Lampe Design", img: "/products/lampes/lampe1.jpeg" },
-  { id: 4, title: "Canapé Confort", img: "/products/canapes/canape1.jpeg" },
-  { id: 5, title: "Étagère Bois", img: "/products/etagere/etagere1.jpeg" },
-  { id: 6, title: "Bureau Minimaliste", img: "/products/bureaux/bureau1.jpeg" },
-  { id: 7, title: "Fauteuil Élégant", img: "/products/fauteuils/fauteuil1.jpeg" },
-  { id: 8, title: "Lit Scandinave", img: "/products/lits/lit1.jpeg" }
-];
+import products from "../../Categorie";
+
 
 const ProductListWithFilters = () => {
+
   return (
     <div className="products_container">
       {/* Section des produits (80%) */}
       <div className="product-list">
         <h2>Nos Produits</h2>
         <div className="product-grid">
-          {products.map((product) => (
-            <div key={product.id} className="product-card">
-              <img src={product.img} alt={product.title} />
-              <h3>{product.title}</h3>
-            </div>
-          ))}
+        {products.flatMap((category) =>
+            category.souscategories.flatMap((subCategory) =>
+              subCategory.produits.map((product, index) => (
+                <div key={index} className="product-card">
+                  <img src={product.images} alt={product.titre} />
+                  <h3>{product.titre}</h3>
+                  <p>{product.description}</p>
+                </div>
+              ))
+            )
+          )}
         </div>
       </div>
-      
+
       {/* Section des filtres (20%) */}
       <div className="products_filters">
         <h2>Filtres</h2>
