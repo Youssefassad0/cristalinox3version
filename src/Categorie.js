993ca7cdcc9e9a173ef0124c7d0e3debe3fad5c9
youssefassad0 <amine150assad@gmail.com>
2025-03-13 07:54:56 +0000

    ajouter list products

--- src/Categorie.js
+++ src/Categorie.js
@@ -1,12 +1,88 @@
 const categories = [
-    { nom: "Mobilier et Décoration intérieure", link: "MDI" },
-    { nom: "Menuiserie", link: "Menuiserie" },
-    { nom: "Équipements de cuisine", link: "EDC" },
-    { nom: "Équipements extérieurs et d'aménagement urbain", link: "EEAU" },
-    { nom: "Composants architecturaux", link: "CA" },
-    { nom: "Équipements sanitaires et industriels", link: "ESI" },
-    { nom: "Transport et véhicules", link: "T&V" },
-    { nom: "Instruments et équipements spécialisés", link: "IES" }
+  {
+    "nom": "Mobilier et Décoration intérieure",
+    "code": "MDI",
+    "sous-categories": [
+      {
+        "nom": "Chaises",
+        "description": "En inox pour un design moderne et épuré.",
+        "produits": [
+          {
+            "titre": "Chaise moderne en inox",
+            "description": "Une chaise élégante en inox avec un design minimaliste.",
+            "images": [
+              "/public/products/chaises/chaise1.jpeg"
+            ]
+          },
+          {
+            "titre": "Chaise 2  moderne en inox",
+            "description": "Une chaise élégante en inox avec un design minimaliste.",
+            "images": [
+              "/public/products/chaises/chaise2.jpeg"
+            ]
+          },
+          {
+            "titre": "Chaise 3 moderne en inox",
+            "description": "Une chaise élégante en inox avec un design minimaliste.",
+            "images": "/public/products/chaises/chaise3.jpeg"
+          }
+        ]
+      },
+      {
+        "nom": "Tables",
+        "description": "Tables à manger, de bureau, ou de café avec des pieds en inox.",
+        "produits": [
+          {
+            "titre": "Table à manger en inox",
+            "description": "Une table robuste et moderne avec un plateau en verre et des pieds en inox.",
+            "images": "/public/products/tables/table1.jpeg"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "nom": "Menuiserie",
+    "code": "Menuiserie",
+    "sous-categories": [
+      {
+        "nom": "Portes",
+        "description": "Portes extérieures ou intérieures en inox, utilisées pour leur robustesse et leur résistance.",
+        "produits": [
+          {
+            "titre": "Porte moderne en inox",
+            "description": "Une porte élégante et résistante pour une sécurité accrue.",
+            "images": [
+              "image7.jpg",
+              "image8.jpg",
+              "image9.jpg"
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "nom": "Équipements extérieurs et d'aménagement urbain",
+    "code": "EEAU",
+    "sous-categories": [
+      {
+        "nom": "Portes",
+        "description": "Portes extérieures ou intérieures en inox, utilisées pour leur robustesse et leur résistance.",
+        "produits": [
+          {
+            "titre": "Porte moderne en inox",
+            "description": "Une porte élégante et résistante pour une sécurité accrue.",
+            "images": [
+              "image7.jpg",
+              "image8.jpg",
+              "image9.jpg"
+            ]
+          }
+        ]
+      }
+    ]
+  }
   ];
   
   export default categories;
--- src/components/Catalogue/Page.jsx
+++ src/components/Catalogue/Page.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import CategorySlider from "./Slider";
+import ListProducts from "./ListProducts";
 
 function Categorie() {
   return (
     <div>
       <h1>Découvrir notre nouveaux Produits</h1>
       <CategorySlider />
-
+      <ListProducts />
     </div>
   );
 }
