d46a26607962e49b5c66cc26e406c755448b3ff2
youssefassad0 <amine150assad@gmail.com>
2025-03-23 18:09:53 +0000

    ajouter lotti to services

--- src/components/about/Services.jsx
+++ src/components/about/Services.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { motion } from "framer-motion";
 import "./Services.css";
+import Lottie from "lottie-react";
+import Delivery from "../../assets/Delivery.json";
+import help from "../../assets/help.json";
+import work from "../../assets/work.json";
 
 function Services() {
   const services = [
     {
       id: 1,
+      icon:help,
+
       title: "Conception et conseil personnalisé",
       description:
         "Accompagnement des clients dès la phase de conception pour choisir les meilleures solutions en inox.",
     },
     {
       id: 2,
+      icon:work,
+
       title: "Fabrication sur commande et sur mesure",
       description:
         "Production d’éléments inox selon les exigences du client avec un contrôle qualité strict.",
     },
     {
       id: 3,
+      icon:Delivery,
       title: "Livraison et installation",
       description:
         "Transport sécurisé des produits jusqu’au site du client avec un emballage soigné pour éviter tout dommage.",
@@ -39,7 +48,11 @@ function Services() {
               transition={{ duration: 0.9, delay: index * 0.3 }} // Décalage progressif
               viewport={{ once: true }} // Animation ne se déclenche qu'une seule fois
             >
-              <div className="icon-service">{service.id}</div>
+              {/* <div className="icon-service">{service.id}</div> */}
+              <Lottie animationData={service.icon} 
+              style={{ fill: "white" }}
+              className="icon-service" />
+
               <div className="content-service">
                 <h3>{service.title}</h3>
                 <p>{service.description}</p>
